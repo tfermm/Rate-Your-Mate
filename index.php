06dd5f86bce0af3e90f63a32c282a50327fa16f4
Kris Reynolds <krmreynolds@gmail.com>
2011-10-23 15:17:38 -0400

    added

--- index.php
+++ index.php
@@ -3,7 +3,13 @@
 include ("includes/login.php");
 include ("includes/menu.php");
 
-
+$_SESSION['roster'] = array(
+    array("screenname" => "kmreynolds1", "name" => "Kris Reynolds", "id" => 1),
+    array("screenname" => "sn1", "name" => "Tyler Fermis", "id" => 2),
+    array("screenname" => "sn2", "name" => "Kris1", "id" => 3),
+    array("screenname" => "sn3", "name" => "Kris2", "id" => 4),
+    array("screenname" => "sn4", "name" => "Kris3", "id" => 5),
+)
  
 ?>
 
