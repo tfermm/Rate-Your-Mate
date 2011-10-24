8e39aea802d5100a82194c94ed867c30e2e6f897
Kris Reynolds <krmreynolds@gmail.com>
2011-10-24 14:26:42 -0400

    Fixed some instructor bugs, preparing for list

--- index.php
+++ index.php
@@ -5,10 +5,15 @@ include ("includes/menu.php");
 
 $_SESSION['roster'] = array(
     array("screenname" => "kmreynolds1", "name" => "Kris Reynolds", "id" => 1),
-    array("screenname" => "sn1", "name" => "Tyler Fermis", "id" => 2),
-    array("screenname" => "sn2", "name" => "Kris1", "id" => 3),
-    array("screenname" => "sn3", "name" => "Kris2", "id" => 4),
-    array("screenname" => "sn4", "name" => "Kris3", "id" => 5),
+    array("screenname" => "bpbielicki", "name" => "Ben Bielicki", "id" => 2),
+    array("screenname" => "mdecota", "name" => "Michael Decota", "id" => 3),
+    array("screenname" => "jdennett", "name" => "Jason Dennett", "id" => 4),
+    array("screenname" => "tferm", "name" => "Tyler Ferm", "id" => 5),
+    array("screenname" => "khallock", "name" => "Keith Hallock", "id" => 6),
+    array("screenname" => "rameden", "name" => "Ryley Ameden", "id" => 7),
+    array("screenname" => "cbecker", "name" => "Chris Becker", "id" => 8),
+    array("screenname" => "jcormier", "name" => "Joe Cormier", "id" => 9),
+    array("screenname" => "cstoner", "name" => "Cara Stoner", "id" => 10)
 )
  
 ?>
--- instructor_setup.php
+++ instructor_setup.php
@@ -6,7 +6,7 @@ error_reporting(-1);
 	<title> Instructor Setup </title>
 	
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
-	<script src="js/behavior.js"> </script>
+	<script src="js/instructor_setup.js"> </script>
 
 </head>
 <body>
@@ -23,27 +23,38 @@ error_reporting(-1);
 		      action="instruct_submit.asp" method="post"  >
 			Project I.D.: <input type="text" name="projectID" /> 	<br />
 			Number of Groups: 
-			<?php // Have a spinner that will increment, passes the max/min value in with it, so can resuse the JS for mulitple spinners ?>
+			
 			<table cellpadding="0" cellspacing="0" border="0">
 				<tr>
-					<td rowspan="2"><input  type="text" id="groupText" name="numGroups" value="1" <?php //style="width:50px;height:23px;font-weight:bold;" ?> /></td>
-					<td><input type="button" id="btnAdd" value=" /\ " style="font-size:7px;margin:0;padding:0;width:20px;height:13px;" ></td>
+					<td rowspan="2"><input  type="text" id="groupText" name="numGroups" value="1"  /></td>
+					<td><input type="button" id="btnAdd" value=" /\ " 
+						style="font-size:7px;margin:0;padding:0;width:20px;height:13px;"/ ></td>
 				</tr>
 				<tr>
-					<td><input type="button" id="btnDel" value=" \/ " style="font-size:7px;margin:0;padding:0;width:20px;height:12px;" /></td>
+					<td><input type="button" id="btnDel" value=" \/ " 
+						style="font-size:7px;margin:0;padding:0;width:20px;height:12px;" /></td>
 				</tr>
 			</table>
 
-			<form id="createGroups">
-				<div id="input1" class="clonedInput">
-					Group: <input type="text" name="group1" id="group1" />
-				</div>
-				
-				<div>
+			<div id="rosterSource" >
+				<select name="roster" multiple="multiple">
+				<?php
+				foreach($_SESSION['roster'] as &$student ) {
+					echo "<option value=".$student['screenname']." id=".$student['id'].">";
+					echo $student['name']."</option>";
+				}
+				?>
+				</select>
+			</div>
 
-				</div>
-			</form>
-			
+			<div id="input1" class="clonedInput">
+				
+				<select name="group1" size="4" id="group1" multiple="multiple">
+				</select>
+				
+				<?php //Group: <input type="text" name="group1" id="group1" /> ?>
+			</div>
+						
 			<br />
 			<p>Who creates the Contract? </p>
 			<br />
