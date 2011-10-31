59e2dfa15f0470de3c6e093c1a2aeddff513fdbc
Keith Hallock <krhallock@plymouth.edu>
2011-10-31 16:21:21 -0400

    simple html

---
+++ student_input.php
@@ -0,0 +1,181 @@
+
+
+<html>
+
+
+<div id="head">
+
+<head>
+
+
+
+
+<title>Rate Your Mate</title>
+
+
+
+
+</head>
+
+
+
+
+
+
+
+
+
+	
+
+	
+
+<h2>Student Input</h2>
+	
+
+
+</div>
+
+
+
+
+
+<div id="menu">
+
+
+<?php include ("includes/menu.php"); ?>
+
+
+
+</div>
+
+
+
+
+
+
+
+<div id="content">
+
+
+<p align="left">
+
+
+<strong>Student Input<strong>
+
+</br>
+
+<font size="2" >Please evaluate your group members on each behavior in your contract.</font>
+
+</br>
+
+</p>
+
+
+
+
+
+<p align="left">
+<strong>Behavior One</strong>
+
+
+
+</p>
+
+</br>
+
+<form action="process.php" method="POST"> 
+
+<textarea wrap="virutal" name="comments" rows="5" cols="50">Student One</textarea>
+
+<input type="submit" value="Submit Comments">
+
+</form>
+
+
+
+</br>
+
+
+<form action="process.php" method="POST"> 
+
+<textarea wrap="virutal" name="comments" rows="5" cols="50">Student Two</textarea>
+
+<input type="submit" value="Submit Comments">
+
+</form>
+
+</br>
+
+<form action="process.php" method="POST"> 
+
+<textarea wrap="virutal" name="comments" rows="5" cols="50">Student Three</textarea>
+
+<input type="submit" value="Submit Comments">
+
+</form>
+
+
+
+</br>
+
+<p align="left">
+<strong>Behavior Two</strong>
+
+</br>
+
+<form action="process.php" method="POST"> 
+
+<textarea wrap="virutal" name="comments" rows="5" cols="50">Student one</textarea>
+
+<input type="submit" value="Submit Comments">
+
+</form>
+
+
+
+</br>
+
+</br>
+
+<form action="process.php" method="POST"> 
+
+<textarea wrap="virutal" name="comments" rows="5" cols="50">Student Two</textarea>
+
+<input type="submit" value="Submit Comments">
+
+</form>
+
+
+
+</br>
+
+</br>
+
+<form action="process.php" method="POST"> 
+
+<textarea wrap="virutal" name="comments" rows="5" cols="50">Student Three</textarea>
+
+<input type="submit" value="Submit Comments">
+
+</form>
+
+
+
+</br>
+
+
+<p align="left">
+
+<strong>Overall</strong>
+
+</p>
+
+<textarea rows="5" cols="50">Holder for Pie Chart!!1!1</textarea>
+
+
+
+
+</div>
+
+</body>
+</html>	
