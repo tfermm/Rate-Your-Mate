887b08937db5e9d8f4013ab12eef79220334e94f
Keith Hallock <krhallock@plymouth.edu>
2011-10-31 16:50:46 -0400

    html edits

--- create_contract.php
+++ create_contract.php
@@ -25,6 +25,15 @@
 
 <div id="content">
 
+<p>Project I.D.</p>
+
+<select>
+  <option value="prj">Project ID</option>
+  
+</select>
+
+<br/>
+
 <p> How many behaviors will your contract contain? </p>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
