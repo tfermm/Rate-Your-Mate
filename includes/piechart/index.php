3ba868dbe3d830887769a19737a1ecd585dd9a08
Tyler Ferm <tfermm@gmail.com>
2011-11-14 17:19:47 -0500

    see previous commit

--- includes/piechart/index.php
+++ includes/piechart/index.php
@@ -1,12 +1,12 @@
-﻿<!-- http://dmitry.baranovskiy.com/work/fiechart/ --!>
+﻿<!-- http://dmitry.baranovskiy.com/work/fiechart/ -->
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<title>Pie Chart (JavaScript edition)</title>
-	<style type="text/css" media="screen">
-		@import"piechart.css";
+	 <style type="text/css" media="screen">
+	 	@import "piechart.css";
 	</style>
-	<!--[if lt IE 7]>
+<!--[if lt IE 7]>
 		<style type="text/css" media="screen">
 			@import"piechart-ie.css";
 		</style>
@@ -14,6 +14,7 @@
 	<script type="text/javascript" charset="utf-8" src="piechart.js"></script>
 	<script type="text/javascript" charset="utf-8" src="ajax.js"></script>
 	<script type="text/javascript" charset="utf-8" src="local.js"></script>
+	<script type="text/javascript" charset="utf-8" src="sliderSynch.js"></script>
 </head>
 <body>
 	<div id="pie">
@@ -22,71 +23,56 @@
 	</div>
 	<form action="#no_javascript-no_fun" id="pie-form">
 		<div>
-			<input type="hidden" id="test" value="2, 5, 10, 1" />
+		
+		
+      
+		<?php include"input.php"?>        
+
+			
+			
+			
+			
 			<img src="!.png" id="valid" alt="!" />
 			<div>
-				<button type="submit">
-					<img src="go.png" alt="Update" title="update" />
-				</button>
+				<!-- <button type="submit"> -->
+				<!-- 	<img src="go.png" alt="Update" title="update" /> -->
+				<!-- </button> -->
+				
+
 
 				<button type="button" id="perc">
 					<img src="percent.png" alt="Percentage view on/off" title="percentage view on/off" />
 				</button>
 
 				
-				<button type="button" id="rot-cc">
-				<!-- 	<img src="rot2.png" alt="Rotate clockwise" title="rotate clockwise" /> -->
-				</button>
-				<button type="button" id="rot-acc">
-				<!--	<img src="rot1.png" alt="Rotate anti-clockwise" title="rotate anti-clockwise" /> -->
-				</button> 
+				<!-- this is just here cuz the js script is looking for those values -->
+				<div id="rot-cc"></div>
+				<div id="rot-acc"></div>
+			
+				
+				
+				
+				<?php include"sliders.php"?>
+				
+				
+				
+				
+				
+				
+				
+				
 				
 				<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
-          <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
-          <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
-          <style type="text/css">
-            #slider { margin: 10px; }
-          </style>
+         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+         <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+          
           <!-- example im using atm http://www.ryancoughlin.com/2008/11/04/using-the-jquery-ui-slider/#4 -->
-          <script>
-            $(document).ready(function() {
-              $("#slider1").slider({
-              handle: '#slider-handle',
-              slide: var mypos = $('#slider-bar').slider("value"),
-              min:0,
-              max:20,
-              
-              
-            })
-            });
-          </script>
-        </head>
-        <body style="font-size:62.5%;">
-  
-        <div id="slider"></div>
+          
         
         
-        <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
-          <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
-          <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
-          <style type="text/css">
-            #slider { margin: 10px; }
-          </style>
-          <script>
-            $(document).ready(function() {
-              $("#slider2").slider(){
-              slide: $value2 = ui.value;              
-              min: 0'
-              max: 20'
-              
-            }
-              
-            });
-          </script>
-        </head>
-        <body style="font-size:62.5%;">
-  
-        <div id="slider"></div>
+
+
+
 				
 				
 				
@@ -94,7 +80,7 @@
 				
 				
 				
-				<!-- <?php include("sliders.php"); ?> will add this after I get a slider working the way it should first -->
+	
 				
 				
 
--- includes/piechart/sliders.php
+++ includes/piechart/sliders.php
@@ -1,30 +1,106 @@
 <?php
+//need to grab maxPoints and groupSize from the data base.
+//$maxPoints = 44;
+//$groupSize = 11;
 
-  $groupSize = 4;
-  echo '<br>';
-  for ($i=1; $i<=$groupSize; $i++)
-  {
-    echo ('<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>');
-    echo   ('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>');
-    echo   '<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>';
-    echo     '<style type="text/css">';
-    echo     '#slider { margin: 10px; }';
-    echo   '</style>';
-    
-    echo '<div id="names">';
-    echo  $i;
-    echo '</div>';
-    echo '<div id="slider' . $i . '">';
-    echo  '<script>';
- 
-    echo   '$(document).ready(function() {';
-    echo     '$("#slider' . $i . '").slider();';
-    echo   '});';
-    echo   '</script>';
-    echo '</head>';
-    echo '<body style="font-size:62.5%;">';
-  
-    echo '</div>';
-    echo ('<br>'); 
-  }
+$output = '';
+for ($j=0; $j < $groupSize-1; $j++){
+  $output = $output . 'pos[' . ($j) . ']+ ", " + ';    
+}
+$output = $output . 'pos[' . ($j) . "];\n";
+
 ?>
+
+<!-- much of this pie chart was based off of the pie chart found at http://dmitry.baranovskiy.com/work/fiechart/ -->
+<style type="text/css">
+.sliders { margin: 1.5em;}
+</style>
+
+<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+<script type="text/javascript">
+var maxPoints = <?php echo $maxPoints; ?>;
+var groupSize = <?php echo $groupSize; ?>;
+var pos = new Array();
+var old_pos = new Array();
+
+$(document).ready(function() {
+  var test = $('#test');
+  var pc = new PieChart();
+
+  createSliders(<?php echo $groupSize;?>, <?php echo $maxPoints; ?>);
+  <?php
+      
+  if ($maxPoints%$groupSize == 0)
+  {
+    $firstNumb = floor($maxPoints/$groupSize); 
+  }
+  else
+  { 
+    $firstNumb = $maxPoints-(floor($maxPoints/$groupSize)*($groupSize-1));
+  }
+  
+  echo 'pos[0] = ' . $firstNumb . ";\n";
+  echo '$( "#slider0" ).slider( "value", pos[0]);' . "\n";
+  
+  for ($k=1; $k < $groupSize; $k++)
+  {
+    echo  "pos[$k] = " . floor($maxPoints/$groupSize) . ";\n";
+    echo '$( "#slider' . $k . '" ).slider( "value", pos[' . $k . ']);' . "\n";
+  }
+  ?>
+
+  pc.setValues(pos);
+  test.value = pos;
+  
+  });
+</script>
+
+</head>
+
+<body style="font-size:62.5%;">
+<?php
+for($i=0; $i<$groupSize; $i++) {
+  ?>    
+  <div id="slider<?php echo $i;?>" class="sliders"></div>
+  <?php
+}
+?>
+<br/>
+  <?php
+
+  if($groupSize==1){
+    //If group size == 1
+      echo   '<style type="text/css">' . "\n";
+      echo        '#slider { margin: 10px; }' . "\n";
+      echo      '</style>' . "\n";
+              
+      echo      '<script>' . "\n";
+      echo        '$(document).ready(function() {' . "\n";
+      echo          '$("#slider' . $groupSize . '").slider({' . "\n";
+      	
+      echo          'slide: function (e, ui){';
+      echo            'var pos' . $groupSize . ' = ui.value;' . "\n"; //sets the variable pos to the number the slider is at.
+      
+      echo            'test.value = pos' . $groupSize . ';' . "\n";
+             
+      echo            'eval("initial = [" + test.value + "]");' . "\n"; //this line and the next two update the drawing on the piechart
+      echo            'pc.setValues(initial);' . "\n";
+      echo            'test.value = initial;' . "\n";
+      echo          '},' . "\n";
+    
+      echo          'min:0,' . "\n";  //min value of the slider
+      echo          'max:' . $maxPoints . ',' . "\n";  //max value
+      echo        '})' . "\n";
+      echo        '});';
+      echo      '</script>' . "\n";
+      echo     '</head>' . "\n";
+      echo    '<body style="font-size:62.5%;">' . "\n";
+      
+      echo    '<div id="slider' . $groupSize . '" class="sliders"></div>' . "\n";
+      echo '<br>' . "\n";
+  
+  
+  
+  }
